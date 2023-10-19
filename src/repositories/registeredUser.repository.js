1b47fe5f5855117553668c11f628a8f63699b1fb
Ernesto Miranda <ernomitrovic@zohomail.com>
2023-10-19 14:05:39 -0600

    Team service added

--- src/repositories/registeredUser.repository.js
+++ src/repositories/registeredUser.repository.js
@@ -6,8 +6,11 @@ class RegisteredUserRepository extends BaseRepository {
     super(RegisteredUser);
     _registeredUser = RegisteredUser;
   }
-
+  
   //async funcs 
+  async updateLeader(id, leader_status, team_id) {
+    return await super.update(id, {is_leader: leader_status, team_id: team_id});
+  }
 }
 
 module.exports = RegisteredUserRepository;
--- src/services/team.service.js
+++ src/services/team.service.js
@@ -1,13 +1,54 @@
+const { generateToken } = require('../helpers/jwt.helper');
 const BaseService = require('./base.service');
 let _TeamRepository = null;
+let _RegisteredUserRepository = null;
 
 class TeamService extends BaseService {
-  constructor({ TeamRepository }) {
+  constructor({ TeamRepository, RegisteredUserRepository=null }) {
     super(TeamRepository);
     _TeamRepository = TeamRepository;
+    _RegisteredUserRepository = RegisteredUserRepository;
   }
 
   //async funcs
+
+  // Create team and update user to leader
+  async create (teamName, userID, isLeader){
+    if(isLeader){
+      const error = new Error();
+      error.message = "Ya eres lider de un equipo";
+      error.status = 401;
+      throw error;
+    }
+    const team = _TeamRepository.create(teamName);
+    const user = await _RegisteredUserRepository.updateLeader(userID, true, team.id);
+    return generateToken(user);
+  }
+  
+  // Update team name
+  async updateName (teamName, teamID, isLeader){
+    if(!isLeader){
+      const error = new Error();
+      error.message = "No eres lider de este equipo";
+      error.status = 401;
+      throw error;
+    }
+    return await _TeamRepository.updateName(teamName, teamID);
+  }
+
+  // Delete team
+  async delete (teamID, isLeader){
+    if(!isLeader){
+      const error = new Error();
+      error.message = "No eres lider de este equipo";
+      error.status = 401;
+      throw error;
+    }
+    await _TeamRepository.delete(teamID);
+    const user = await _RegisteredUserRepository.updateLeader(userID, true, team.id);
+    return generateToken(user);
+  }
+  
 }
 
 
