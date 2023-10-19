88f244177661688852a00e5eb0487370b63f763d
Ernesto Miranda <ernomitrovic@zohomail.com>
2023-10-19 17:53:05 -0600

    JWT generate token actualizado

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -5,6 +5,7 @@ module.exports.generateToken = function(user) {
   const payload = {
     id: user.id,
     is_leader: user.is_leader,
+    team_id: user.team_id,
   }
   return sign(payload, JWT_SECRET, { expiresIn: "4h" });
 };
